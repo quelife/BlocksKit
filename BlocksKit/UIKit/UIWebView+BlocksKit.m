0a0bc05e56a6e2f410fee9835fa55a1560aea711
quelife <thanaphon.que@gmail.com>
2020-12-16 10:19:36 +0700

    Update UIWebView+BlocksKit.m

--- BlocksKit/UIKit/UIWebView+BlocksKit.m
+++ BlocksKit/UIKit/UIWebView+BlocksKit.m
@@ -1,80 +1,80 @@
-//
-//  UIWebView+BlocksKit.m
-//  BlocksKit
-//
+// //
+// //  UIWebView+BlocksKit.m
+// //  BlocksKit
+// //
 
-#import "UIWebView+BlocksKit.h"
-#import "A2DynamicDelegate.h"
-#import "NSObject+A2DynamicDelegate.h"
-#import "NSObject+A2BlockDelegate.h"
+// #import "UIWebView+BlocksKit.h"
+// #import "A2DynamicDelegate.h"
+// #import "NSObject+A2DynamicDelegate.h"
+// #import "NSObject+A2BlockDelegate.h"
 
-#pragma mark Custom delegate
+// #pragma mark Custom delegate
 
-@interface A2DynamicUIWebViewDelegate : A2DynamicDelegate <UIWebViewDelegate>
-@end
+// @interface A2DynamicUIWebViewDelegate : A2DynamicDelegate <UIWebViewDelegate>
+// @end
 
-@implementation A2DynamicUIWebViewDelegate
+// @implementation A2DynamicUIWebViewDelegate
 
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-	BOOL ret = YES;
+// - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+// 	BOOL ret = YES;
 	
-	id realDelegate = self.realDelegate;
-	if (realDelegate && [realDelegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)])
-		ret = [realDelegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
+// 	id realDelegate = self.realDelegate;
+// 	if (realDelegate && [realDelegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)])
+// 		ret = [realDelegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
 
-	BOOL (^block)(UIWebView *, NSURLRequest *, UIWebViewNavigationType) = [self blockImplementationForMethod:_cmd];
-	if (block)
-		ret &= block(webView, request, navigationType);
+// 	BOOL (^block)(UIWebView *, NSURLRequest *, UIWebViewNavigationType) = [self blockImplementationForMethod:_cmd];
+// 	if (block)
+// 		ret &= block(webView, request, navigationType);
 
-	return ret;
-}
+// 	return ret;
+// }
 
-- (void)webViewDidStartLoad:(UIWebView *)webView {
-	id realDelegate = self.realDelegate;
-	if (realDelegate && [realDelegate respondsToSelector:@selector(webViewDidStartLoad:)])
-		[realDelegate webViewDidStartLoad:webView];
+// - (void)webViewDidStartLoad:(UIWebView *)webView {
+// 	id realDelegate = self.realDelegate;
+// 	if (realDelegate && [realDelegate respondsToSelector:@selector(webViewDidStartLoad:)])
+// 		[realDelegate webViewDidStartLoad:webView];
 
-	void (^block)(UIWebView *) = [self blockImplementationForMethod:_cmd];
-	if (block) block(webView);
-}
+// 	void (^block)(UIWebView *) = [self blockImplementationForMethod:_cmd];
+// 	if (block) block(webView);
+// }
 
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-	id realDelegate = self.realDelegate;
-	if (realDelegate && [realDelegate respondsToSelector:@selector(webViewDidFinishLoad:)])
-		[realDelegate webViewDidFinishLoad:webView];
+// - (void)webViewDidFinishLoad:(UIWebView *)webView {
+// 	id realDelegate = self.realDelegate;
+// 	if (realDelegate && [realDelegate respondsToSelector:@selector(webViewDidFinishLoad:)])
+// 		[realDelegate webViewDidFinishLoad:webView];
 
-	void (^block)(UIWebView *) = [self blockImplementationForMethod:_cmd];
-	if (block) block(webView);
-}
+// 	void (^block)(UIWebView *) = [self blockImplementationForMethod:_cmd];
+// 	if (block) block(webView);
+// }
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-	id realDelegate = self.realDelegate;
-	if (realDelegate && [realDelegate respondsToSelector:@selector(webView:didFailLoadWithError:)])
-		[realDelegate webView:webView didFailLoadWithError:error];
+// - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+// 	id realDelegate = self.realDelegate;
+// 	if (realDelegate && [realDelegate respondsToSelector:@selector(webView:didFailLoadWithError:)])
+// 		[realDelegate webView:webView didFailLoadWithError:error];
 
-	void (^block)(UIWebView *, NSError *) = [self blockImplementationForMethod:_cmd];
-	if (block) block(webView, error);
-}
+// 	void (^block)(UIWebView *, NSError *) = [self blockImplementationForMethod:_cmd];
+// 	if (block) block(webView, error);
+// }
 
-@end
+// @end
 
-#pragma mark Category
+// #pragma mark Category
 
-@implementation UIWebView (BlocksKit)
+// @implementation UIWebView (BlocksKit)
 
-@dynamic bk_shouldStartLoadBlock, bk_didStartLoadBlock, bk_didFinishLoadBlock, bk_didFinishWithErrorBlock;
+// @dynamic bk_shouldStartLoadBlock, bk_didStartLoadBlock, bk_didFinishLoadBlock, bk_didFinishWithErrorBlock;
 
-+ (void)load
-{
-	@autoreleasepool {
-		[self bk_registerDynamicDelegate];
-		[self bk_linkDelegateMethods:@{
-			@"bk_shouldStartLoadBlock": @"webView:shouldStartLoadWithRequest:navigationType:",
-			@"bk_didStartLoadBlock": @"webViewDidStartLoad:",
-			@"bk_didFinishLoadBlock": @"webViewDidFinishLoad:",
-			@"bk_didFinishWithErrorBlock": @"webView:didFailLoadWithError:"
-		}];
-	}
-}
+// + (void)load
+// {
+// 	@autoreleasepool {
+// 		[self bk_registerDynamicDelegate];
+// 		[self bk_linkDelegateMethods:@{
+// 			@"bk_shouldStartLoadBlock": @"webView:shouldStartLoadWithRequest:navigationType:",
+// 			@"bk_didStartLoadBlock": @"webViewDidStartLoad:",
+// 			@"bk_didFinishLoadBlock": @"webViewDidFinishLoad:",
+// 			@"bk_didFinishWithErrorBlock": @"webView:didFailLoadWithError:"
+// 		}];
+// 	}
+// }
 
-@end
+// @end
